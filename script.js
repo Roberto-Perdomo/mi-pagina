1d214a9927686fe5ecf35a7e0a8d39d7542421d2
Roberto-Perdomo <desktoprobe@gmail.com>
2025-06-15 14:02:42 -0500

    Actualizo archivos sin alterar commits anteriores

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function saludar(){
-    alert("Hola, bienvenido a mi sitio web!");
-    console.log("El usuario ha sido saludado.");
-}
+document.getElementById("miboton").addEventListener("click", async () => {
+  const respuesta = await fetch("http://localhost:3000/mensaje", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ mensaje: "Hola desde el botón" }),
+  });
+
+  const data = await respuesta.json();
+  alert("Respuesta del servidor: " + data.recibido);
+});
